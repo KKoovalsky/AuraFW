ce373a67c49e178976f1909d5578b26fd4c31451
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-09-22 12:38:13 +0200

    Add cmake function to download and populate Catch2

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,1 +1,12 @@
+function(ProvideCatch2)
 
+    Include(FetchContent)
+
+    FetchContent_Declare(
+      Catch2
+      GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+      GIT_TAG        efd8cc8777406457a34e15313076ac3a77d4e54b)
+
+    FetchContent_MakeAvailable(Catch2)
+
+endfunction()
