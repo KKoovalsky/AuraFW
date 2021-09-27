2a31d9625ac3c4526995b5e017dc878c2cde2ed7
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-09-27 10:16:23 +0200

    Add toolchain file to compile host side with clang
    This will:
    1. Add function that downloads LLVM+Clang 12.0.0 into
    'build/common_dependencies' directory, to not download it for each build
    configuration, but once and for all build configurations (for host,
    device build; for release and debug builds).
    2. Add toolchain file which sets the compiler paths to the downloaded
    toolchain and adds initial compile flags for C/C++ to emit more errors.
    
    Note that the `CMAKE_TOOLCHAIN_FILE` must be set before the `project`
    command. CMake evaluates the variable during `project` command.
    
    We can download the LLVM into a common directory and unpack it there,
    because it is a tool run on host, and the target architecture doesn't
    affect it.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -10,3 +10,27 @@ function(ProvideCatch2)
     FetchContent_MakeAvailable(Catch2)
 
 endfunction()
+
+function(ProvideLlvm)
+
+    Include(FetchContent)
+
+    set(COMMON_DEPENDENCIES_PATH ${CMAKE_BINARY_DIR}/../common_dependencies)
+
+    FetchContent_Declare(
+        Llvm
+        URL https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
+        URL_HASH SHA256=a9ff205eb0b73ca7c86afc6432eed1c2d49133bd0d49e47b15be59bbf0dd292e
+        PREFIX ${COMMON_DEPENDENCIES_PATH}
+        SUBBUILD_DIR ${COMMON_DEPENDENCIES_PATH}/llvm-subbuild
+        SOURCE_DIR ${COMMON_DEPENDENCIES_PATH}/llvm-src
+        BINARY_DIR ${COMMON_DEPENDENCIES_PATH}/llvm-build
+    )
+
+    FetchContent_MakeAvailable(Llvm)
+
+    FetchContent_GetProperties(Llvm SOURCE_DIR LLVM_TOOLCHAIN_SOURCE_DIR)
+
+    set(LLVM_TOOLCHAIN_PATH "${LLVM_TOOLCHAIN_SOURCE_DIR}" CACHE PATH "Path to the LLVM toolchain") 
+
+endfunction()
