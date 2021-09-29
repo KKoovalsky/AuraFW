ee7b0f57296e0439130003782244da7362e74b75
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-09-29 09:39:56 +0200

    Fix scan-build static analysis
    The previous approach didn't work properly, because the CC and CXX
    supplied by scan-build were not used. After changing the toolchain file
    and redesigning the 'clang_static_analysis' target to run cmake's
    generate and configure step with scan-build too, it will work.
    Previously the generate and configure step were not run under
    scan-build. This meant that CC and CXX were not injected to CMake, but
    to Make instead (only to the build step). In turn, CC and CXX were not
    replaced at all.

--- cmake/utilities.cmake
+++ cmake/utilities.cmake
@@ -1,24 +1,34 @@
 macro(EnableClangStaticAnalysis)
 
-    add_test(
-        # Rebuild the entire, cleaned project on-the-side with scan-build.
-        NAME clang_static_analysis 
-        COMMAND ${CMAKE_CTEST_COMMAND} 
-            # 'Build myself' by taking the main CMakeLists.txt
-            --build-and-test ${CMAKE_SOURCE_DIR} ${CMAKE_BINARY_DIR}/clang_static_analysis
-            # This is a hack: effectively, will skip the Build stage by not performing implicit clean, but
-            # we invoke it anyway. We could not use '--build-noclean', but then 'clean' would be called twice.
-            --build-noclean
-            --build-target clean
-            --build-generator ${CMAKE_GENERATOR}
-            # scan-build needs Debug configuration
-            --build-options -DCMAKE_BUILD_TYPE=Debug
-            --test-command 
-                ${LLVM_TOOLCHAIN_PATH}/bin/scan-build 
-                    -v -v -o reports 
+    set(on_side_build_path ${CMAKE_BINARY_DIR}/clang_static_analysis)
+    set(scan_build_path ${LLVM_TOOLCHAIN_PATH}/bin/scan-build)
+    set(reports_path ${CMAKE_BINARY_DIR}/clang_static_analysis_reports)
+
+    # Creates clean directory where the analysis will be built.
+    add_custom_target(clang_static_analysis_prepare
+        COMMAND ${CMAKE_COMMAND} -E rm -rf ${on_side_build_path}
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${on_side_build_path}
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    )
+
+    # Runs the analysis from the path created specifically for that task
+    add_custom_target(clang_static_analysis
+        # scan-build wants Debug build, for better analysis.
+        COMMAND ${scan_build_path} ${CMAKE_COMMAND} ${CMAKE_SOURCE_DIR} -DCMAKE_BUILD_TYPE=Debug
+        COMMAND ${scan_build_path}
+                    -v -v -o ${reports_path} 
                     -enable-checker optin.cplusplus.UninitializedObject 
                     -enable-checker optin.cplusplus.VirtualCall
                     ${CMAKE_COMMAND} --build .
+        WORKING_DIRECTORY ${on_side_build_path}
+    )
+    
+    # Run the *_prepare target always before the analysis
+    add_dependencies(clang_static_analysis clang_static_analysis_prepare)
+
+    add_test(
+        NAME clang_static_analysis
+        COMMAND ${CMAKE_COMMAND} --build . --target clang_static_analysis
     )
 
 endmacro()
