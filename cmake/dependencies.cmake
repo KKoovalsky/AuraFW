b5dd9d6b7c36c0f413b0f378dd0233a98c144c5b
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-10-14 14:10:56 +0200

    Upgrade to LLVM 13.0.0
    clang-tidy had a bug in version 12.0.0:
    https://bugs.llvm.org/show_bug.cgi?id=46235
    
    13.0.0 fixes it.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -22,8 +22,8 @@ function(ProvideLlvm)
 
     FetchContent_Declare(
         Llvm
-        URL https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
-        URL_HASH SHA256=a9ff205eb0b73ca7c86afc6432eed1c2d49133bd0d49e47b15be59bbf0dd292e
+        URL https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.0/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
+        URL_HASH SHA256=2c2fb857af97f41a5032e9ecadf7f78d3eff389a5cd3c9ec620d24f134ceb3c8
         PREFIX ${COMMON_DEPENDENCIES_PATH}
         SUBBUILD_DIR ${COMMON_DEPENDENCIES_PATH}/llvm-subbuild
         SOURCE_DIR ${COMMON_DEPENDENCIES_PATH}/llvm-src
