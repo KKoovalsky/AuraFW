9b3956e66099b203727c247ac7a76a6a7b239268
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-10-20 19:47:25 +0200

    Simplify Loggable
    I was too enthusiastic about Concepts and overengineered Loggable
    concept. From the interface point of view, the MeasurementScheduler
    needs it to use it like that:
    
        logger(LogLevel::info) << "some number: " << 2;
    
    And that's it. No more magic, disabling implicit conversion and return
    value specification. Those are details, and the implementer class is
    responsible for the details.

--- src/common/loggable.hpp
+++ src/common/loggable.hpp
@@ -17,32 +17,14 @@ enum class LogLevel
 };
 
 template<typename T>
-concept HasLogMethodReturningReferenceToSelf = requires(T v)
+concept Loggable = requires(T v, unsigned unsigned_, int int_, float float_, unsigned char unsigned_char_, char char_)
 {
-    {
-        v.log(LogLevel{})
-        } -> std::convertible_to<T&>;
+    v << "string literal" //
+      << unsigned_        //
+      << int_             //
+      << float_           //
+      << unsigned_char_   //
+      << char_;           //
 };
-
-//! Disallows implicit cast of basic types, allowing only string-literals and string-like to std::string implicit cast.
-template<HasLogMethodReturningReferenceToSelf T, class U>
-requires(!std::convertible_to<U, std::string>) auto operator<<(T, U) = delete;
-
-template<typename T>
-concept HasOutputStreamOperatorOverloadsForBasicTypes =
-    requires(T v, unsigned unsigned_, int int_, float float_, unsigned char unsigned_char_, char char_)
-{
-    {
-        v << "string literal" //
-          << unsigned_        //
-          << int_             //
-          << float_           //
-          << unsigned_char_   //
-          << char_            //
-        } -> std::convertible_to<T&>;
-};
-
-template<typename T>
-concept Loggable = HasLogMethodReturningReferenceToSelf<T> && HasOutputStreamOperatorOverloadsForBasicTypes<T>;
 
 #endif /* LOGGABLE_HPP */
