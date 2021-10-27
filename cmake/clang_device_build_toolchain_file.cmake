b11b0f09b1b2253df5d6917f747a64bced02e8ba
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-10-27 09:00:51 +0200

    Add toolchain file for the device build
    The toolchain file uses LLVM Toolchain that is configured to suck the
    standard libraries from ARM GNU Toolchain. The toolchain file is almost
    the same like for the host-side build except:
    
    * system name is "Generic" and no Processor is defined
    * 'set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)' is used to pass
    the CMake compiler checks. Otherwise, some symbols will be missing
    during compilation.
    * architecture related compile flags are added to C, CXX and ASM flags:
    " -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16"
    * clang is configured to use precompiled libraries from ARM GNU
    Toolchain: " -nostdlib -nostdlib++",
    " --sysroot=${ARM_GNU_TOOLCHAIN_PATH}/sysroot"
    * optimization flags are enabled: -fdata-sections, -ffunction-sections,
    --gc-sections

---
+++ cmake/clang_device_build_toolchain_file.cmake
@@ -0,0 +1,47 @@
+set(CLANG_COMPILER_PATH_PREFIX ${LLVM_TOOLCHAIN_PATH}/bin)
+
+# Without that you will get linking error related to no _exit symbol when CMake tests gcc compile.
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_SYSTEM_NAME Generic)
+
+if(DEFINED ENV{CC})
+    set(CMAKE_C_COMPILER $ENV{CC})
+else()
+    set(CMAKE_C_COMPILER ${CLANG_COMPILER_PATH_PREFIX}/clang)
+endif()
+
+if(DEFINED ENV{CXX})
+    set(CMAKE_CXX_COMPILER $ENV{CXX})
+else()
+    set(CMAKE_CXX_COMPILER ${CLANG_COMPILER_PATH_PREFIX}/clang++)
+endif()
+
+set(CMAKE_ASM_COMPILER ${CLANG_COMPILER_PATH_PREFIX}/clang)
+set(CMAKE_LINKER ${CLANG_COMPILER_PATH_PREFIX}/lld)
+
+set(CMAKE_AR ${CLANG_COMPILER_PATH_PREFIX}/llvm-ar)
+set(CMAKE_OBJCOPY ${CLANG_COMPILER_PATH_PREFIX}/llvm-objcopy)
+set(CMAKE_OBJCOPY_BIN ${CLANG_COMPILER_PATH_PREFIX}/llvm-objcopy)
+set(CMAKE_RANLIB ${CLANG_COMPILER_PATH_PREFIX}/llvm-ranlib)
+set(CMAKE_STRIP ${CLANG_COMPILER_PATH_PREFIX}/llvm-strip)
+set(CMAKE_SIZE_BIN ${CLANG_COMPILER_PATH_PREFIX}/llvm-size)
+
+string(CONCAT basic_flags
+    " -Wall -Wextra"
+    " -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16"
+    " -nostdlib -nostdlib++"
+    " --sysroot=${ARM_GNU_TOOLCHAIN_PATH}/sysroot")
+
+set(object_file_generation_flags "-fdata-sections -ffunction-sections")
+set(compile_c_flags "${basic_flags} ${object_file_generation_flags}")
+set(extra_linker_flags "-Wl,--gc-sections")
+
+set(CMAKE_C_FLAGS_INIT "${compile_c_flags}")
+set(CMAKE_ASM_FLAGS_INIT  "${basic_flags}")
+set(CMAKE_CXX_FLAGS_INIT "${compile_c_flags}")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "${basic_flags} ${extra_linker_flags}")
+
+set(CMAKE_C_COMPILER_TARGET arm-none-eabi)
+set(CMAKE_CXX_COMPILER_TARGET arm-none-eabi)
+set(CMAKE_ASM_COMPILER_TARGET arm-none-eabi)
