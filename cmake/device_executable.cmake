3e733a2e538e686e945753f6daf3015eee52a40b
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-10-27 13:48:16 +0200

    Allow to create device executables
    This will add a CMake function which is able to create executables that
    are strictly targeted to our device. That includes:
    
    * using the linker script generated by CubeMX
    * using the startup file generated by CubeMX
    * creating "-flash" target that can be used to flash firmware
    
    Miscellanous:
    * generation of the HEX file
    * printing binary size after build is done
    
    To compile the firmware correctly I had to define the _init() function.
    Otherwise lld will fail linking this function, because it is never
    defined. I guess that GCC generates it on its own. I have checked the
    _init() function generated by GCC and it looks like that:
    
        push {r3, r4, r5, r6, r7, lr}
        nop
        pop	{r3, r4, r5, r6, r7}
        pop	{r3}
        mov	lr, r3
        bx	lr
    
    Basically it does nothing: calls to push and pop balance each other,
    effectively doing no action.

---
+++ cmake/device_executable.cmake
@@ -0,0 +1,52 @@
+function(MakeDeviceExecutable target_name)
+
+    LinkDeviceSpecificDetails(${target_name})
+    GenerateHexAfterBuild(${target_name})
+    PrintBinarySizeAfterBuild(${target_name})
+    AddFlashTarget(${target_name})
+
+endfunction()
+
+function(LinkDeviceSpecificDetails target_name)
+
+    set(cube_generated_files_dir "${PROJECT_ROOT_DIR}/device/cube/Aura")
+    set(startup_file "${cube_generated_files_dir}/startup_stm32l432xx.s")
+    set(clang_init_file "${PROJECT_ROOT_DIR}/device/clang_init.c")
+
+    add_library(device_specific STATIC ${startup_file} ${clang_init_file})
+
+    set(linker_script "${cube_generated_files_dir}/STM32L432KCUx_FLASH.ld")
+    target_link_options(${target_name} PRIVATE -T${linker_script})
+
+    target_link_libraries(${target_name} PRIVATE device_specific)
+
+endfunction()
+
+function(GenerateHexAfterBuild target_name)
+
+    set(target_hex_file ${CMAKE_CURRENT_BINARY_DIR}/${target_name}.hex)
+    add_custom_command(TARGET ${target_name} POST_BUILD
+        COMMAND ${CMAKE_OBJCOPY} -O ihex $<TARGET_FILE:${target_name}> ${target_hex_file}
+        BYPRODUCTS ${target_hex_file}
+        COMMENT "Generating executable HEX file for ${target_name}"
+    )
+
+endfunction()
+
+function(PrintBinarySizeAfterBuild target_name)
+
+    add_custom_command(TARGET ${target_name} POST_BUILD
+        COMMAND ${CMAKE_SIZE_BIN} $<TARGET_FILE:${target_name}>
+        COMMENT "Section sizes of the executable binary"
+    )
+
+endfunction()
+
+function(AddFlashTarget target_name)
+
+    find_program(OPENOCD_PROGRAM NAMES openocd REQUIRED)
+    add_custom_target(${target_name}-flash
+        COMMAND ${OPENOCD_PROGRAM} -f board/stm32l4discovery.cfg -c "program $<TARGET_FILE:${target_name}> reset exit"
+    )
+
+endfunction()
