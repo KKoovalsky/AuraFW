5395c7d4a3557abe8e163b747ce8f8f536848894
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-11-01 12:35:33 +0100

    Add sketch for serial logger tests
    This will add only the description of the requirements to the serial
    logger.
    
    We use the freshly created CreateDeviceTest() to create executable for
    device-side testing.

---
+++ tests/device/test_serial_logger/test_serial_logger.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file    test_serial_logger.cpp
+ * @brief   Tests the SerialLogger
+ * @author  Kacper Kowalski - kacper.s.kowalski@gmail.com
+ */
+
+// =====================================================================================================================
+// Test cases declaration
+// =====================================================================================================================
+void test_simple_log_is_printed();
+void test_multiple_logs_are_printed();
+void test_enormous_number_of_logs_doesnt_clog_the_logger();
+void test_multiple_instances_of_logger_can_be_created_simultaneously();
+void test_multiple_instances_of_logger_can_be_created_non_simultaneously();
+
+// =====================================================================================================================
+// Test cases definition
+// =====================================================================================================================
+void test_simple_log_is_printed()
+{
+}
+
+void test_multiple_logs_are_printed()
+{
+}
+
+void test_enormous_number_of_logs_doesnt_clog_the_logger()
+{
+}
+
+void test_multiple_instances_of_logger_can_be_created_simultaneously()
+{
+}
+
+void test_multiple_instances_of_logger_can_be_created_non_simultaneously()
+{
+}
