3b82d1ade55ab6ba4433bcc750d5997c30d29fe3
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-11-02 11:33:18 +0100

    Add two more test descriptions for Loggable check

--- tests/device/test_serial_logger/test_serial_logger.cpp
+++ tests/device/test_serial_logger/test_serial_logger.cpp
@@ -8,15 +8,21 @@
 // Test cases declaration
 // =====================================================================================================================
 void test_simple_log_is_printed();
+void test_implements_loggable();
 void test_multiple_logs_are_printed();
 void test_enormous_number_of_logs_doesnt_clog_the_logger();
 void test_multiple_instances_of_logger_can_be_created_simultaneously();
 void test_multiple_instances_of_logger_can_be_created_non_simultaneously();
+void test_cannot_use_output_operator_without_calling_log_first();
 
 // =====================================================================================================================
 // Test cases definition
 // =====================================================================================================================
 void test_simple_log_is_printed()
+{
+}
+
+void test_implements_loggable()
 {
 }
 
@@ -33,5 +39,9 @@ void test_multiple_instances_of_logger_can_be_created_simultaneously()
 }
 
 void test_multiple_instances_of_logger_can_be_created_non_simultaneously()
+{
+}
+
+void test_cannot_use_output_operator_without_calling_log_first()
 {
 }
