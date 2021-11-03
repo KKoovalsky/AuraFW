360f5df4b942ee0cbfc723c467af56af73b053b7
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-11-03 12:16:16 +0100

    Add test description for logging other types

--- tests/device/test_serial_logger/test_serial_logger.cpp
+++ tests/device/test_serial_logger/test_serial_logger.cpp
@@ -17,7 +17,12 @@ void test_enormous_number_of_logs_doesnt_clog_the_logger();
 void test_multiple_instances_of_logger_can_be_created_simultaneously();
 void test_multiple_instances_of_logger_can_be_created_non_simultaneously();
 void test_cannot_use_output_operator_without_calling_log_first();
-
+void test_float_is_printed_with_precision_to_thousandths();
+void test_unsigned_char_is_printed_as_hex_char();
+void test_unsigned_int_is_printed();
+void test_negative_int_is_printed();
+void test_positive_int_is_printed();
+void test_single_char_is_printed();
 // =====================================================================================================================
 // Test cases definition
 // =====================================================================================================================
@@ -55,5 +60,29 @@ void test_multiple_instances_of_logger_can_be_created_non_simultaneously()
 }
 
 void test_cannot_use_output_operator_without_calling_log_first()
+{
+}
+
+void test_float_is_printed_with_precision_to_thousandths()
+{
+}
+
+void test_unsigned_char_is_printed_as_hex_char()
+{
+}
+
+void test_unsigned_int_is_printed()
+{
+}
+
+void test_negative_int_is_printed()
+{
+}
+
+void test_positive_int_is_printed()
+{
+}
+
+void test_single_char_is_printed()
 {
 }
