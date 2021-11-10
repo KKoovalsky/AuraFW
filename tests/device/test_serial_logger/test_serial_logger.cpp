2359beac3940975cddddc45b02483d3f1cbcedb3
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-11-10 16:05:13 +0100

    Test for multiple logs printing over serial logger

--- tests/device/test_serial_logger/test_serial_logger.cpp
+++ tests/device/test_serial_logger/test_serial_logger.cpp
@@ -11,7 +11,6 @@
 // Test cases declaration
 // =====================================================================================================================
 void test_implements_loggable();
-void test_multiple_logs_are_printed();
 void test_enormous_number_of_logs_doesnt_clog_the_logger();
 void test_multiple_instances_of_logger_can_be_created_simultaneously();
 void test_multiple_instances_of_logger_can_be_created_non_simultaneously();
@@ -31,25 +30,6 @@ void test_error_log_is_printed();
 void test_implements_loggable()
 {
     static_assert(Loggable<SerialLogger>);
-}
-
-void test_multiple_logs_are_printed()
-{
-    bool is_success{false};
-    try
-    {
-        SerialLogger serial_logger{};
-        serial_logger.log(LogLevel::info) << "some log1";
-        serial_logger.log(LogLevel::info) << "some log2";
-        serial_logger.log(LogLevel::info) << "some log3";
-        serial_logger.log(LogLevel::info) << "some log4";
-        serial_logger.log(LogLevel::info) << "some log5";
-        serial_logger.log(LogLevel::info) << "some log6";
-        is_success = true;
-    } catch (const std::exception&)
-    {
-    }
-    TEST_ASSERT(is_success);
 }
 
 void test_enormous_number_of_logs_doesnt_clog_the_logger()
